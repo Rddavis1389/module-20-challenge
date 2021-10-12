1b792ca54e3f2e51ee22a456f8f016a9637a24e1
Rddavis1389 <rddavis1389@yahoo.com>
2021-10-11 20:47:31 -0600

    profile

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <a href="https://github.com/Rddavis1389">Github</a>
+      <p>rddavis1389@yahoo.com</p>
+      <p>(915)242-3313</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>About Me</h1>
+      <p>
+        My name is Randolph but I go by Randy. I was born and raised in San
+        Antonio, TX. After highschool I received an football scholarship to
+        OPSU. After my knee injuries i transferred to UTEP where I studied
+        Mechanical Engineering. I am Currently working in construction around
+        the Oil fields in Texas and New Mexico.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
